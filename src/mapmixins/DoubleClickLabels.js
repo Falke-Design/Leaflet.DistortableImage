15798b11f685a030646c73103477175805fef771
Florian Bischof <design.falke@gmail.com>
2024-05-12 12:30:23 +0200

    Fix Bug

--- src/mapmixins/DoubleClickLabels.js
+++ src/mapmixins/DoubleClickLabels.js
@@ -2,6 +2,6 @@ L.Map.mergeOptions({
   doubleClickLabels: false,
 });
 
-L.Map.DoubleClickLabels = L.Map.Handler.extend({});
+L.Map.DoubleClickLabels = L.Handler.extend({});
 
 L.Map.addInitHook('addHandler', 'doubleClickLabels', L.Map.DoubleClickLabels);
